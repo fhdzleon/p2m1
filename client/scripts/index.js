6e76837075f64b3519ac777c687126460455ed5c
fhdzleon <fhdzleon@gmail.com>
2024-03-03 00:25:10 -0600

    peticiones API

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,84 +1,72 @@
- 
-//? --> Funcion para mostrar citas random
+// --> Funcion para mostrar citas random
 
 const displayRandomQuote = (quotes) => {
-    const displayQuote = document.getElementById("randomQuote") 
-    const randomIndex = Math.floor(Math.random() * quotes.length) 
-    let quote = quotes[randomIndex]
+  const displayQuote = document.getElementById("randomQuote");
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  let quote = quotes[randomIndex];
 
-    displayQuote.textContent = quote;
-}
+  displayQuote.textContent = quote;
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    displayRandomQuote(quotes);
-    buildMovies();
+document.addEventListener("DOMContentLoaded", function () {
+  displayRandomQuote(quotes);
+  buildMovies();
 });
 
-//? Definimos las clases que necesitaremos -->
+// --> Funcion creadora de cards
 
-class Movie {
-    constructor(title,year, director, duration, genre, rate, poster) {
-        this.title = title;
-        this.year = year;
-        this.director = director;
-        this.duration = duration;
-        this.genre = genre;
-        this.rate = rate;
-        this.poster = poster;
-    }
+function buildMovie(data) {
+  const { title, year, director, duration, genre, rate, poster } = data;
+
+  const domTitle = document.createElement("h2");
+  domTitle.innerHTML = title;
+
+  const domPoster = document.createElement("img");
+  domPoster.src = poster;
+
+  const domDirector = document.createElement("p");
+  domDirector.innerHTML = `<b>Director: </b> ${director}`;
+
+  const domYear = document.createElement("p");
+  domYear.innerHTML = `<b>Año: </b> ${year}`;
+
+  const domDuration = document.createElement("p");
+  domDuration.innerHTML = `<b>Duración: </b> ${duration}`;
+
+  const domGenre = document.createElement("p");
+  domGenre.innerHTML = `<b>Género: </b> ${genre.join(" - ")}`;
+
+  const domRate = document.createElement("p");
+  domRate.innerHTML = `<b>Calificación: </b> ${rate}⭐`;
+
+  // CONTENEDOR DE LOS DATOS -->
+  const container = document.createElement("div");
+  container.className = "card";
+
+  // ARAMDO DE CARD -->
+
+  container.append(
+    domTitle,
+    domPoster,
+    domDirector,
+    domYear,
+    domDuration,
+    domGenre,
+    domRate
+  );
+
+  return container;
 }
 
-//? --> Funcion creadora de cards
+// --> Funcion para solicitar datos de api y armar todas las cards.
 
-    function buildMovie(tempData) {
-        const {title,year, director, duration, genre, rate, poster} = tempData;
+function buildMovies() {
+  const containerMovies = document.getElementById("recs");
 
-        const domTitle = document.createElement("h2");
-        domTitle.innerHTML = title;
-
-        const domPoster = document.createElement("img");
-        domPoster.src = poster;
-
-        const domDirector = document.createElement("p");
-        domDirector.innerHTML = `<b>Director: </b> ${director}`
-
-        const domYear = document.createElement("p");
-        domYear.innerHTML = `<b>Año: </b> ${year}`
-
-        const domDuration = document.createElement("p");
-        domDuration.innerHTML = `<b>Duración: </b> ${duration}`
-
-        const domGenre = document.createElement("p");
-        domGenre.innerHTML = `<b>Género: </b> ${genre.join(' - ')}`
-
-        const domRate = document.createElement("p");
-        domRate.innerHTML = `<b>Calificación: </b> ${rate}⭐`
-
-        // CONTENEDOR DE LOS DATOS -->
-        const container = document.createElement('div')
-        container.className = "card"
-
-        // ARAMDO DE CARD -->
-
-        container.appendChild(domTitle)
-        container.appendChild(domPoster)
-        container.appendChild(domDirector)
-        container.appendChild(domYear)
-        container.appendChild(domDuration)
-        container.appendChild(domGenre)
-        container.appendChild(domRate)
-
-        return container;
-    }
-
-    function buildMovies() {
-        const containerMovies = document.getElementById("recs")
-
-        const renderMovies = tempData.map((movie) => 
-            buildMovie(movie)
-        );
-
-        renderMovies.forEach((renderMovie) => 
-            containerMovies.appendChild(renderMovie)
-        )
-    }
+  $.get("https://webpt19b.web.app/data/movies.json", (data) => {
+    data.forEach((movie) => {
+      const renderMovie = buildMovie(movie);
+      containerMovies.appendChild(renderMovie);
+    });
+  });
+}
